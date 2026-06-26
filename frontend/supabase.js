cafae74e6bc6d434ee7e02de3557cc5039259c7a
unknown <tkkserviceteam@gmail.com>
2026-06-26 22:42:20 +0800

    update

--- frontend/supabase.js
+++ frontend/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const db = createClient(
-  "https://mzmkdexhkbkpgdvfzazs.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16bWtkZXhoa2JrcGdkdmZ6YXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0Njg4MTgsImV4cCI6MjA5ODA0NDgxOH0.eKznfYuZy5h-5NXma6CAYRdZJucPrf-dlYolByH-Lfs"
-);
+const supabaseUrl = "https://mzmkdexhkbkpgdvfzazs.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16bWtkZXhoa2JrcGdkdmZ6YXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0Njg4MTgsImV4cCI6MjA5ODA0NDgxOH0.eKznfYuZy5h-5NXma6CAYRdZJucPrf-dlYolByH-Lfs";
+
+export const db = createClient(supabaseUrl, supabaseKey);
